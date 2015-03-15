e5abec58c724f03e6fb92f5b8986ff4221a21223
miss157998 <peter19940622@gmail.com>
2015-03-15 23:17:36 +0800

    cascade final ver
    增加了手動控制 跟 debug

--- cascad/cascad/cascad.cpp
+++ cascad/cascad/cascad.cpp
@@ -18,14 +18,26 @@ int main(){
 	double F[17000],D[17000],Ftarget,e,*alpha,th;
 	int n[17001], fdc, correctCount, countk, countj, counte, correctflage,thCount;
 	int i,count,vCount,vm,vl,ecuCount;
+	int conf_non_auto_num,conf_sCount[100],non_auto_key,conf_counter; //config file: non auto num , how many solider at current layer and the minima acceptable detection rate at current layer
+	double cascade_alpha[17000],cascade_th[17000];
+	double conf_dRate[100];
 	IntImg *P, *N,*V;
 	Soldier **h;
 	char str[256];
 	Soldier **strong =nullptr;
 	Soldier **cascadedStrong = nullptr;
-	FILE *vSet,*out;
+	FILE *vSet,*out,*conf; // conf : configration file
 	fopen_s(&out, "out.txt", "w");
 	fopen_s(&vSet, "vSetIntegralImage", "rb");
+	fopen_s(&conf,"config.txt","r");
+	fscanf_s(conf,"%d",&conf_non_auto_num);
+	cout << "conf non auto num:" << conf_non_auto_num << endl;
+	for (i = 0; i < conf_non_auto_num; i++){
+		fscanf_s(conf, "%d", &conf_sCount[i]);
+		fscanf_s(conf, "%lf", &conf_dRate[i]);
+		cout << i + 1 << " , conf_sCount: " << conf_sCount[i] << " , conf_dRate: " << conf_dRate[i] << endl;
+	}
+	fclose(conf);
 	if (!vSet)cout << "vSet error" << endl;
 	fread(&vCount, sizeof(int), 1, vSet);//±qÀÉ®×Åª¨ú´ú¸Õ¥Î¼Ë¥»
 	fread(&vm, sizeof(int), 1, vSet);
@@ -44,8 +56,7 @@ int main(){
 	ecuCount = 0;// example can use, means how many nonfase sample were been delete at the last stage
 	while (F[i] > Ftarget){
 		cout << "-----------------  stage  " << i <<"  -----------------"<<endl;
-		if (i == 2)
-			cout << "here!"<<endl;
+		
 		fprintf_s(out, "%d\n", i);
 		i++;
 		n[i] = 0; // so, n is start by 1;   the number of features , ni , of each stage;
@@ -59,18 +70,45 @@ int main(){
 			w[count] = ex[count].isFace ? (double)1 / (2 * m) : (double)1 / (2 * l);
 		}
 		th = 0;
+		
 		while (F[i] > f*F[i - 1]){
 		//while (n[i]<2){
-			n[i]++;
+			non_auto_key = 0;
+			if (conf_non_auto_num != 0){
+				conf_non_auto_num--;
+				non_auto_key = 1;
+			}
+
+			if (non_auto_key)
+				n[i] = conf_sCount[i-1];
+			else
+				n[i]++;
+			
 			cout << "face : " << m << " nonface : " << l << "ecuCount : " << ecuCount<<endl; 
-			training(n[i], strong);
-			strong[n[i]-1]->getData(str);
-			//cout << str << endl;
-			e = strong[n[i] - 1]->getE();
-			alpha[n[i]-1] = log((1 - e) / e);
-			//for (th = 0, count = 0; count < n[i]; count++)
-			//	th += alpha[count] / 2;
-			th += alpha[n[i] - 1] / 2;
+
+			if (non_auto_key){
+				for (conf_counter = 1; conf_counter<=conf_sCount[i - 1]; conf_counter++)
+					training(conf_counter, strong);
+			}else
+				training(n[i], strong);
+
+			if (non_auto_key){
+				for (conf_counter = 0; conf_counter < conf_sCount[i - 1]; conf_counter++){
+					e = strong[conf_counter ]->getE();
+					alpha[conf_counter] = log((1 - e) / e);
+					th += alpha[conf_counter] / 2;
+				}
+			}
+			else{
+				//strong[n[i]-1]->getData(str);
+				//cout << str << endl;
+				e = strong[n[i] - 1]->getE();
+				alpha[n[i] - 1] = log((1 - e) / e);
+				for (th = 0, count = 0; count < n[i]; count++)
+					th += alpha[count] / 2;
+				//th += alpha[n[i] - 1] / 2;
+			}
+
 			thCount = 0;
 			while (1){
 				for (count = 0, fdc = 0, correctCount = 0; count < vCount; count++){
@@ -82,17 +120,32 @@ int main(){
 				}
 				D[i] = (double)correctCount / vm;
 				F[i] = (double)fdc / vl;
-				if (D[i] < d*D[i - 1]){
-					th *= 0.99;
-					thCount++;
-					if (thCount>1000)th = 0;
-			}
-				else
-					break;
+				if (non_auto_key){
+					if (D[i] < conf_dRate[i - 1]){
+						th *= 0.99;
+						thCount++;
+						if (th<0.001)th = 0;
+					}
+					else
+						break;
+				}
+				else{
+					if (D[i] < d*D[i - 1]){
+						th *= 0.99;
+						thCount++;
+						if (thCount>1000)th = 0;
+					}
+					else
+						break;
+				}
 			}
 			cout << "F[i] > f * F[i-1] : " << F[i] << " > " << f << " * " << F[i - 1] << " , " << f*F[i - 1] << endl;
-		}
+			cout << "F[i] : " << F[i] << " , D[i] : " << D[i] <<" , th : "<<th<< endl;
+			if (non_auto_key)
+				break;
 
+		}
+		cascade_th[i - 1] = th;
 		cascadedStrong[i - 1] = (Soldier*)malloc(sizeof(Soldier)*n[i]);
 		for (count = 0; count < n[i]; count++){//storage strong into cascaded strong
 			cascadedStrong[i - 1][count] = *strong[count];
@@ -100,7 +153,7 @@ int main(){
 			cout << str << endl;
 			fprintf_s(out, "%s\n", str);
 		}
-		cout << "F : " << F[i] << " D : " << D[i] << endl;
+		cout << "F : " << F[i] << " D : " << D[i] <<" th : "<<cascade_th[i-1]<< endl;
 		
 		if (F[i]>Ftarget){
 			for ( counte = 0 , ecuCount = 0 ; counte < eCount; counte++){
@@ -110,14 +163,14 @@ int main(){
 					continue;
 				
 				
-					for (countj = 0; countj < n[i]; countj++){
+					/*for (countj = 0; countj < n[i]; countj++){
 						e = cascadedStrong[i-1][countj].getE();
 						alpha[countj] = log((1 - e) / e);
 					}
 					for (countk = 0, th = 0; countk < n[i]; countk++)
-						th += alpha[countk] / 2;
+						th += alpha[countk] / 2;*/
 					correctflage = 0;
-					if (judge(ex + counte, strong, alpha, th, n[i]) - (int)ex[counte].isFace == 0)
+					if (judge(ex + counte, strong, alpha, cascade_th[i-1], n[i]) - (int)ex[counte].isFace == 0)
 						correctflage = 1;
 					else
 						correctflage = 0;
