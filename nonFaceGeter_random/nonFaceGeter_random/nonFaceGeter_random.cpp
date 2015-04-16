339d32f7e3c2fab2a0156007aca5cd201cd40417
mdes4214 <mdes4214@gmail.com>
2015-04-17 00:08:30 +0800

    (4/17) nonfaceGeter_random update
    release問題解決啦~~(灑花

--- nonFaceGeter_random/nonFaceGeter_random/nonFaceGeter_random.cpp
+++ nonFaceGeter_random/nonFaceGeter_random/nonFaceGeter_random.cpp
@@ -10,16 +10,12 @@ using namespace std;
 
 #define WIDTH 24
 #define HEIGHT 24
-#define RAND_CYCLE 10
+#define RAND_CYCLE 100
 
 int main(void)
 {
 	srand(time(NULL));
 
-	IplImage *pSrcImg = NULL, *pNewSrcImg = NULL;
-	IplImage *pDstImg = NULL;
-	CvMat *mat;
-	CvRect rect;
 	char srcName[256] = { 0 }, dstName[256] = { 0 }, stageDirName[256] = "F:\\not_face\\stage00", tmpStr[256] = { 0 }, sysStageDir[256] = { 0 }, dstNum[256] = { 0 };
 	char sysMove[256] = { 0 }, sysRename[256] = { 0 };
 	FILE *config, *config_imgBackup;
@@ -27,6 +23,7 @@ int main(void)
 	DIR *dir = NULL, *tmpDir = NULL;
 	dirent *entry = NULL;
 
+	
 	if (fopen_s(&config_imgBackup, "F:\\config_imgBackup.txt", "r+") != 0){
 		fopen_s(&config_imgBackup, "F:\\config_imgBackup.txt", "w+");
 		fclose(config_imgBackup);
@@ -35,6 +32,7 @@ int main(void)
 	if (fscanf_s(config_imgBackup, "%d", &imgBackupNum) == EOF){
 		fprintf_s(config_imgBackup, "0");
 	}
+	
 
 	if (fopen_s(&config, "F:\\config.txt", "r+") != 0){
 		fopen_s(&config, "F:\\config.txt", "w+");
@@ -48,7 +46,7 @@ int main(void)
 		fscanf_s(config, "%d", &dstCount[i]);
 
 
-	CvSize size = cvSize(WIDTH, HEIGHT), size2 = cvSize(WIDTH * 5, HEIGHT * 5);
+	Size size = Size(WIDTH, HEIGHT), size2 = Size(WIDTH * 5, HEIGHT * 5);
 
 	if ((dir = opendir("F:\\not_face")) == NULL)
 		system("mkdir F:\\not_face");
@@ -73,6 +71,8 @@ int main(void)
 		sprintf_s(dstName, stageDirName);
 		int len = strlen(dstName);
 		while (1){
+			Mat SrcImg;
+
 			if (!(entry = readdir(dir))){
 				endFlag = 1;
 				break;
@@ -84,30 +84,28 @@ int main(void)
 				strstr(entry->d_name, ".png") ||
 				strstr(entry->d_name, ".PNG")){
 				sprintf_s(srcName, "F:\\image\\%s", entry->d_name);
-				pSrcImg = cvLoadImage(srcName, 1);
+				SrcImg = imread(srcName);
 				entry = NULL;
 			}
 			else{
 				entry = NULL;
 				continue;
 			}
-			/*
+			
 			sprintf_s(sysRename, "rename %s image_%07d.jpg", srcName, imgBackupNum);
-			system(sysRename);*/
+			system(sysRename);
 			sprintf_s(srcName, "F:\\image\\image_%07d.jpg", imgBackupNum++);
 			sprintf_s(sysMove, "move %s %s", srcName, "F:\\image_backup_test");
 			system(sysMove);
-			/*
-			if (!(pSrcImg->width < size2.width || pSrcImg->height < size2.height)){
-				pNewSrcImg = cvCreateImage(size2, pSrcImg->depth, pSrcImg->nChannels);
-				cvResize(pSrcImg, pNewSrcImg, CV_INTER_LINEAR);
-			}
-			else
-				pNewSrcImg = cvCloneImage(pSrcImg);
-			*/
 			for (int index = 0; index < RAND_CYCLE; index++){
-				int sW = rand() % pSrcImg->width, sH = rand() % pSrcImg->height, 
-					lW = rand() % pSrcImg->width, lH = rand() % pSrcImg->height;
+				int sW, sH, lW, lH;
+
+				do{
+					sW = rand() % SrcImg.cols;
+					sH = rand() % SrcImg.rows;
+					lW = rand() % SrcImg.cols;
+					lH = rand() % SrcImg.rows;
+				} while (sW == lW || sH == lH);
 				int tmp;
 
 				if (sW > lW){
@@ -120,63 +118,21 @@ int main(void)
 					sH = lH;
 					lH = tmp;
 				}
-
 				int h = lH - sH, w = lW - sW;
-				CvSize size3 = cvSize(h, w);
-				mat = cvCreateMat(h, w, CV_8UC3);
-				pDstImg = cvCreateImage(size3, IPL_DEPTH_8U, 3);
-				IplImage *tmpDstImg = cvCloneImage(pDstImg), *pNewDstImg = cvCreateImage(size, IPL_DEPTH_8U, 3);
+				Size size3 = Size(h, w);
+				Rect rect = Rect(sW, sH, w, h);
+				Mat DstImg = SrcImg(rect), newDstImg;
 
-				rect = cvRect(sW, sH, w, h);
-				cvGetSubRect(pSrcImg, mat, rect);
-				cvGetImage(mat, pDstImg);
-				cvResize(pDstImg, pNewDstImg, CV_INTER_LINEAR);
-
+				resize(DstImg, newDstImg, size, INTER_AREA);
 				sprintf_s(dstNum, "\\not_face_No_%06d.bmp", dstCount[stageNum - 1]++);
 				strcat_s(dstName, dstNum);
-				cvSaveImage(dstName, pNewDstImg);
+				imwrite(dstName, newDstImg);
 				dstName[len] = '\0';
 
-				pDstImg = cvCloneImage(tmpDstImg);
-				cvReleaseImage(&pDstImg);
-				cvReleaseImage(&tmpDstImg);
-				cvReleaseImage(&pNewDstImg);
-				cvReleaseMat(&mat);
+				DstImg.release();
+				newDstImg.release();
 			}
-			/*
-			for (int x = 0; x + WIDTH <= pNewSrcImg->width; x += WIDTH){
-				for (int y = 0; y + HEIGHT <= pNewSrcImg->height; y += HEIGHT){
-					mat = cvCreateMat(HEIGHT, WIDTH, CV_8UC3);
-					pDstImg = cvCreateImage(size, IPL_DEPTH_8U, 3);
-					IplImage *tmpDstImg = cvCloneImage(pDstImg);
-
-					rect = cvRect(x, y, WIDTH, HEIGHT);
-					cvGetSubRect(pNewSrcImg, mat, rect);
-					cvGetImage(mat, pDstImg);
-
-					sprintf_s(dstNum, "\\not_face_No_%06d.bmp", dstCount[stageNum - 1]++);
-					strcat_s(dstName, dstNum);
-					cvSaveImage(dstName, pDstImg);
-					dstName[len] = '\0';
-
-					pDstImg = cvCloneImage(tmpDstImg);
-					cvReleaseImage(&pDstImg);
-					cvReleaseImage(&tmpDstImg);
-					cvReleaseMat(&mat);
-				}
-			}
-			*/
-			/*
-			cvReleaseImage(&pNewSrcImg);
-			pNewSrcImg = cvCreateImage(size, pSrcImg->depth, pSrcImg->nChannels);
-			cvResize(pSrcImg, pNewSrcImg, CV_INTER_LINEAR);
-			sprintf_s(dstNum, "\\not_face_No_%06d.bmp", dstCount[stageNum - 1]++);
-			strcat_s(dstName, dstNum);
-			cvSaveImage(dstName, pNewSrcImg);
-			dstName[len] = '\0';
-			*/
-			//cvReleaseImage(&pNewSrcImg);
-			cvReleaseImage(&pSrcImg);
+			SrcImg.release();
 
 			cout << "image" << ++index << " complete." << endl;
 			if (index >= 5000){
@@ -195,11 +151,11 @@ int main(void)
 	for (int i = 0; i < stageNum; i++)
 		fprintf_s(config, "%d\n", dstCount[i]);
 	fclose(config);
-	/*
+	
 	rewind(config_imgBackup);
 	fprintf_s(config_imgBackup, "%d\n", imgBackupNum);
-	*/
 	fclose(config_imgBackup);
+	
 
 	return 0;
 }
